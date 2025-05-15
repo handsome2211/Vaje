446d5f34d8dfe706314bfdd6a848ab09706f9648
handsome2211 <jan.muzga1993@gmail.com>
2025-05-15 09:13:23 +0200

    Dokončal nalogo

--- vaja2/main.js
+++ vaja2/main.js
@@ -14,18 +14,41 @@ function resetiraj(event){
     }
 
     
-    console.log("ljudi je bilo",+ stej);
-    shraniST.innerHTML = stej;
-    stej = 0;
-    steviloLjudi.innerHTML = 0;
+    kliki = kliki + 1;
+    console.log(kliki);
+
+    if (kliki == 1){
+        console.log("ljudi je bilo",+ stej);
+        shraniST.innerHTML = stej +",";
+        stej = 0;
+        steviloLjudi.innerHTML = 0;
+    }
+    
+    else if (kliki == 2 ){
+        console.log("ljudi je bilo",+ stej);
+        shraniST2.innerHTML = stej + ",";
+        stej = 0;
+        steviloLjudi.innerHTML = 0;
+    }
+
+    else if (kliki == 3){
+        console.log("ljudi je bilo"+ stej);
+        shraniST3.innerHTML = stej ;
+        stej = 0;
+        steviloLjudi.innerHTML = 0;
+        kliki = 0;
+    }
 
 }
 
 shraniST = document.getElementById("kolicina");
+shraniST2 = document.getElementById("kolicina2");
+shraniST3 = document.getElementById("kolicina3");
 steviloLjudi = document.getElementById("stevilo");
 dodaj = document.getElementById("btn1");
 shrani = document.getElementById("btn2");
 var stej = 0;
+var kliki = 0;
 
 dodaj.addEventListener("click",sestevaj);
 shrani.addEventListener("click",resetiraj);       
