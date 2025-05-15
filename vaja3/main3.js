c135d498facaedfbeac69a060042a67403ebb9af
handsome2211 <jan.muzga1993@gmail.com>
2025-05-15 10:56:17 +0200

    vaja 3

--- vaja3/main3.js
+++ vaja3/main3.js
@@ -1,10 +1,20 @@
 function filmi(){
 
+    array = [];
     var vrednost = stevilo.value;
     console.log("tvojih najljubsih filmo je ",+ vrednost);
+
+    for(var i = 0; i < vrednost ; i++){
+        array.push(i);
+    }
+    element.classList.remove("vloga");
+    element.classList.add("pokazi");
+    console.log(array);
 }
 
 var btn = document.getElementById("gumb");
 var stevilo = document.getElementById("Stfilmi");
+var array = [];
+var element = document.getElementById("inputi");
 
 btn.addEventListener("click",filmi);
