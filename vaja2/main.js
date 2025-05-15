f875ed982267b1537b12ccb80a4b9be7965d6d56
handsome2211 <jan.muzga1993@gmail.com>
2025-05-15 08:59:04 +0200

    Naredil nalogo

--- vaja2/main.js
+++ vaja2/main.js
@@ -8,9 +8,24 @@ function sestevaj(event){
     steviloLjudi.innerHTML = stej;
 }
 
+function resetiraj(event){
+    if(event){
+        event.preventDefault();
+    }
+
+    
+    console.log("ljudi je bilo",+ stej);
+    shraniST.innerHTML = stej;
+    stej = 0;
+    steviloLjudi.innerHTML = 0;
+
+}
+
+shraniST = document.getElementById("kolicina");
 steviloLjudi = document.getElementById("stevilo");
 dodaj = document.getElementById("btn1");
 shrani = document.getElementById("btn2");
 var stej = 0;
 
 dodaj.addEventListener("click",sestevaj);
+shrani.addEventListener("click",resetiraj);       
