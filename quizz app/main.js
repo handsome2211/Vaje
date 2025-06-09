556c872d1f3f2675efe518153555d1669e3f5c9d
handsome2211 <jan.muzga1993@gmail.com>
2025-06-09 16:48:12 +0200

    yes

--- quizz app/main.js
+++ quizz app/main.js
@@ -5,8 +5,13 @@ const questions = [
     answer: "Paris",
   },
   {
-    question: "What is the capital of france",
-    options: ["Ljubljana", "Paris", "Madrid", "London"],
-    answer: "Paris",
+    question: "What does 'len()' do in Python?",
+    options: [
+      "Add two numbers",
+      "Returns a lenght of a list or a string",
+      "Prints output to screen",
+      "Defines a variable",
+    ],
+    answer: "Returns a lenght of a list or a string",
   },
 ];
