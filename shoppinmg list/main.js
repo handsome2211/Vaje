90097aede031d37ed1ddd8c533365c6362b00e45
handsome2211 <jan.muzga1993@gmail.com>
2025-06-17 10:49:24 +0200

    Skoraj Končano

--- shoppinmg list/main.js
+++ shoppinmg list/main.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const div = document.getElementById('product');
+const btn = document.querySelector('.cart-btn');
+const menu = document.querySelector('.menu');
 const products = [
   {
     name: 'Wireles mouse',
@@ -34,7 +36,7 @@ const products = [
   },
 ];
 
-products.forEach(element => {
+products.forEach((element, index) => {
   const newDiv = document.createElement('div');
   newDiv.innerHTML = `  
   <h1 class="naslov"> ${element.name} </h1>  
@@ -43,4 +45,37 @@ products.forEach(element => {
   <button class="btn"> Add to Cart</button> `;
   newDiv.classList.add('card');
   div.appendChild(newDiv);
+  const button = newDiv.querySelector('.btn');
+  button.addEventListener('click', function () {
+    klik(index, element);
+  });
 });
+
+btn.addEventListener('click', function () {
+  menu.classList.toggle('menu-show');
+});
+
+document.addEventListener('click', function (event) {
+  if (
+    menu.classList.contains('menu-show') &&
+    !menu.contains(event.target) &&
+    !btn.contains(event.target)
+  ) {
+    menu.classList.toggle('menu-show');
+    console.log(event.target);
+  }
+});
+
+function klik(index, element) {
+  const elementToRemove = menu.querySelector('.prazna');
+  if (elementToRemove) {
+    elementToRemove.remove();
+  }
+  const newDiv = document.createElement('div');
+  newDiv.innerHTML = `
+  <div class="menuName">Name: ${element.name}</div>
+  <div class="menuPrice">Price: ${element.price}$</div>
+  <div class="menuImg">Image: ${element.image}</div> `;
+  menu.appendChild(newDiv);
+  newDiv.classList.add('menu-item');
+}
